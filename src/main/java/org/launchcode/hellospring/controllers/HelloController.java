d1abdcb4f6cbdada4c1359895d025353484fff82
RjCoyle20 <138418419+RjCoyle20@users.noreply.github.com>
2023-09-20 21:00:37 -0500

    Finished exercises

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -23,26 +23,62 @@ public class HelloController {
     public String goodbye() {
         return "Goodbye, Spring!";
     }
+
     // Handles request of the form /hello?name=LaunchCode
     // lives at hello/hello
-  @RequestMapping(method= {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    @RequestMapping(value="hello", method = RequestMethod.POST)
+    @ResponseBody
+    public String helloPost(@RequestParam String name, @RequestParam String language) {
+        if (name == null) {
+            name = "World";
+        }
+
+        return createMessage(name, language);
+
+    }
+    public static String createMessage(String n, String l){
+        String greeting ="";
+
+        if(l.equals("english")) {
+            greeting= "Hello";
+        }
+        else if (l.equals("french")) {
+            greeting = "Bonjour";
+        }
+        else if (l.equals("italian")) {
+            greeting = "Bonjourno";
+        }
+        else if (l.equals("spanish")) {
+            greeting = "Hola";
+        }
+        else if (l.equals("german")) {
+            greeting = "Hallo";
+        }
+
+        return greeting + " " + n;
     }
 
 // Handles requests of the form /hello/Launchcode
-// lives at /hello/form
+//handle requests of the form http://localhost:8080/hello/LaunchCode
     @GetMapping("{name}")
     public String helloWithPathParam(@PathVariable String name){
         return "Hello, " + name + "!";
 
     }
+    // lives at /hello/form
     @GetMapping("form")
     public String helloForm(){
         return "<html>" +
                 "<body>" +
                 "<form action='hello' method='post'>" + //submit a request to /hello
                 "<input type='text' name='name'>" +
+                "<select name='language' id='lang-select'>" +
+                "<option value='english'>English</option>" +
+                "<option value='french'>French</option>" +
+                "<option value='italian'>Italian</option>" +
+                "<option value='spanish'>Spanish</option>" +
+                "<option value='german'>German</option>" +
+                "</select>" +
                 "<input type='submit' value='Greet me!'>" +
                 "</form>" +
                 "</body>" +
